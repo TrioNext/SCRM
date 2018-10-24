853f38093218752412acf2cf640901c592d07f3a
Hồng Hải <honghai.js6@gmail.com>
2018-10-24 14:31:10 +0700

    Update src/hooks/default-params.js

--- src/hooks/default-params.js
+++ src/hooks/default-params.js
@@ -1,7 +1,7 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
-/* 
+/*
 for get default params on FIND METHOD
 */
 
@@ -9,17 +9,8 @@ module.exports = function (options = {}) {
   return async context => {
     const query = context.params.query;
     const paginate = context.app.get('paginate');
-    
-    
-    const query = {
-      p:0,
-      max,
-      sort_by,
-      sort_type
-    }
 
-    console.log(context.params.query)
-    
+
     return context;
   };
 };
