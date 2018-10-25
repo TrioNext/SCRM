b1a2cd38c5d37314fcb51c6f52ecacb03a70b591
Hồng Hải <honghai.js6@gmail.com>
2018-10-25 18:21:10 +0700

    Update src/hooks/filter-on-get-find.js

--- src/hooks/filter-on-get-find.js
+++ src/hooks/filter-on-get-find.js
@@ -4,16 +4,17 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
+    
     let  query  = context.params.query ;
     const paginate = context.app.get('paginate');
 
-    
+
     query.p = query.p || 0 ;
     query.max = query.max || paginate.max ;
     query.sort_by =  query.sort_by || 'id';
     query.sort_type = query.sort_type || 'desc';
 
-    
+
     query.basicQuery = {
         order:[
           [ query.sort_by || 'id' , query.sort_type || 'desc' ]
@@ -26,9 +27,9 @@ module.exports = function (options = {}) {
     delete query.max ;
     delete query.sort_by ;
     delete query.sort_type ;
-    
+
 
     return context;
   };
-  
+
 };
