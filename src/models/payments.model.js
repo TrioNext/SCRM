f3435c9dfcfaf28de5847b767880a00838bc9a56
Hồng Hải <honghai.js6@gmail.com>
2018-10-26 16:03:02 +0700

    Update src/models/payments.model.js

---
+++ src/models/payments.model.js
@@ -0,0 +1,117 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+
+module.exports = function (app) {
+
+  // config Database
+  const sequelize = app.get('sequelizeClient');
+
+  // define table object
+  const payments = sequelize.define('payments',
+    {
+
+     id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull: false,
+        unique: true
+
+     },
+     code:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique: {
+            args: true,
+            msg: 'Vui lòng nhập mã khác'
+        },
+        set(val){
+          this.setDataValue('code',val.toLowerCase())
+        }
+     },
+     name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{
+          notEmpty:{
+            args:true,
+            msg:"Vui lòng nhập tên"
+          },
+          len: [4,100]
+
+        }
+     },
+     type:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        defaultValue:'main'   /* enum [main,sub] : kiểu văn phòng : TM-CK*/
+     },
+     is_deleted:{
+       type:DataTypes.TINYINT,
+       defaultValue:0
+     },
+     status:{
+       type:DataTypes.TINYINT,
+       defaultValue:1      // [1: aenable - 0: disable ]
+     },
+
+     company_id:{
+       type:DataTypes.INTEGER,
+       defaultValue:0
+     },
+     creator_id:{
+       type:DataTypes.INTEGER,
+       defaultValue:0
+     },
+
+     deleted_by:{
+       type:DataTypes.INTEGER,
+       defaultValue:0
+     },
+
+
+     date_created:{
+       type:'TIMESTAMP',
+       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+       allowNull: true
+     },
+     date_modified:{
+       type:'TIMESTAMP',
+       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+       allowNull: true
+     },
+     date_deleted:{
+       type:'TIMESTAMP',
+       allowNull: true
+     },
+
+     total_days_debt_allow:{
+       type:DataTypes.TINYINT,
+       defaultValue:0
+     },
+
+    json:{
+      type:DataTypes.TEXT,
+      allowNull:true,
+      defaultValue:null,
+      get(){
+        var json = this.getDataValue('json');
+        json = json !== null ? json : '{}';
+
+         // 'this' allows you to access attributes of the instance
+        return JSON.parse(json);
+      }
+    }
+
+  },
+    { indexes: [ { unique: true, fields: [ 'code' ] } ] },
+  )
+
+  return payments;
+
+
+
+};
