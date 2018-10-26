34435278355055333f2dde8abab57b9e8216b4f4
Hồng Hải <honghai.js6@gmail.com>
2018-10-26 16:03:08 +0700

    Update src/models/subregions.model.js

---
+++ src/models/subregions.model.js
@@ -0,0 +1,100 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+
+module.exports = function (app) {
+
+  // config Database
+  const sequelize = app.get('sequelizeClient');
+
+  const subregions = sequelize.define('subregions',
+    {
+
+     id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull: false,
+        unique: true
+
+     },
+
+     code:{
+       type:DataTypes.STRING,
+       defaultValue:0
+     },
+     /* mã vùng TP */
+
+     region_id:{
+       type:DataTypes.INTEGER,
+       defaultValue:0
+     },
+     name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{
+          notEmpty:{
+            args:true,
+            msg:"Vui lòng nhập tên"
+          },
+          len: [4,40]
+
+        }
+     },
+
+     type:{
+       type:DataTypes.TINYINT,
+       defaultValue:1
+     },
+     /* [1- quận - 2 : huyện ] */
+
+
+
+     date_created:{
+       type:'TIMESTAMP',
+       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+       allowNull: true
+     },
+     date_modified:{
+       type:'TIMESTAMP',
+       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+       allowNull: true
+     },
+
+
+     lat:{
+       type:DataTypes.FLOAT,
+       defaultValue:0
+     },
+     lng:{
+       type:DataTypes.FLOAT,
+       defaultValue:0
+     },
+
+
+
+
+    json:{
+      type:DataTypes.TEXT,
+      allowNull:true,
+      defaultValue:null,
+      get(){
+        var json = this.getDataValue('json');
+        json = json !== null ? json : '{}';
+
+         // 'this' allows you to access attributes of the instance
+        return JSON.parse(json);
+      }
+    }
+
+  }
+
+  )
+
+  return subregions
+
+
+
+};
