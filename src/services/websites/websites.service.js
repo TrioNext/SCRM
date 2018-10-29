caafb72db5f8203b633c8762fdd69503ab112304
Hồng Hải <honghai.js6@gmail.com>
2018-10-29 17:59:22 +0700

    Update src/services/websites/websites.service.js

---
+++ src/services/websites/websites.service.js
@@ -0,0 +1,22 @@
+// Initializes the `websites` service on path `/websites`
+const createService = require('feathers-sequelize');
+const createModel = require('../../models/websites.model');
+const hooks = require('./websites.hooks');
+
+module.exports = function (app) {
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
+
+  const options = {
+    Model,
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/websites', createService(options));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('websites');
+
+  service.hooks(hooks);
+};
