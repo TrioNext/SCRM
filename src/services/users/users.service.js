59a473b0bfcd3b6f0f71e61c19f81e2f8ff77e6e
Hồng Hải <honghai.js6@gmail.com>
2018-10-30 18:19:23 +0700

    Update src/services/users/users.service.js

--- src/services/users/users.service.js
+++ src/services/users/users.service.js
@@ -1,8 +1,7 @@
 // Initializes the `users` service on path `/users`
 
 /* THIS USER SERVICE OBJECT : WORKINHG WITH DATABASE  */
-const serviceUser = require('./users.class');
-
+const iDB = require('./users.class');
 const hooks = require('./users.hooks');
 const events = require('./users.events');
 
@@ -11,12 +10,11 @@ const events = require('./users.events');
 module.exports = function (app) {
 
 
-  /* ROUTE : /users */
-  app.use('/users',serviceUser({app})) ;
+  /* ROUTE : /users => database service action for response */
+  app.use('/users',iDB(app)) ;
 
   const service = app.service('users');
   service.hooks(hooks);
-
 
 
 };
