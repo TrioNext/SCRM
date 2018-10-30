e2bac263cd52d7aab02a6862188e052f1e6e668f
Hồng Hải <honghai.js6@gmail.com>
2018-10-30 18:18:36 +0700

    Update src/models/brandnames.model.js

---
+++ src/models/brandnames.model.js
@@ -0,0 +1,115 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+
+module.exports = function (app) {
+
+  // config Database
+  const sequelize = app.get('sequelizeClient');
+
+  // define table object
+  const brandnames = sequelize.define('brandnames',
+    {
+
+     id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull: false,
+        unique: true
+
+     },
+
+     name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{
+          notEmpty:{
+            args:true,
+            msg:"Vui lòng nhập tên"
+          },
+          len: [4,100]
+
+        }
+     },
+     /* [1 brandname - partner ]	 */
+     type:{
+       type:DataTypes.TINYINT,
+       defaultValue:1
+     },
+
+     status:{
+       type:DataTypes.TINYINT,
+       defaultValue:1
+     },
+
+     is_deleted:{
+       type:DataTypes.TINYINT,
+       defaultValue:0
+     },
+
+
+     company_id:{
+       type:DataTypes.INTEGER,
+       defaultValue:0
+     },
+
+     creator_id:{
+       type:DataTypes.INTEGER,
+       defaultValue:0
+     },
+
+     deleted_by:{
+       type:DataTypes.INTEGER,
+       defaultValue:0
+     },
+
+     date_created:{
+       type:'TIMESTAMP',
+       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+       allowNull: true
+     },
+
+     date_modified:{
+       type:'TIMESTAMP',
+       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+       allowNull: true
+     },
+
+     date_deleted:{
+       type:'TIMESTAMP',
+       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+       allowNull: true
+     },
+
+     url:{
+       type:DataTypes.STRING,
+       allowNull:true,
+       defaultValue:null
+     },
+     photos:{
+       type:DataTypes.TEXT,
+       allowNull:true,
+       defaultValue:'[]'
+     },
+
+      json:{
+        type:DataTypes.TEXT,
+        allowNull:true,
+        defaultValue:null,
+        get(){
+          var json = this.getDataValue('json');
+          json = json !== null ? json : '{}';
+
+           // 'this' allows you to access attributes of the instance
+          return JSON.parse(json);
+        }
+      }
+    }
+  )
+
+  return brandnames;
+
+};
