d3b1a67b06768066a9a891ff090a4c1e920b39aa
Hồng Hải <honghai.js6@gmail.com>
2018-10-31 18:06:28 +0700

    Update src/services/users/users.hooks.js

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -4,16 +4,20 @@ const {
   hashPassword, protect
 } = require('@feathersjs/authentication-local').hooks;
 
-const filterOnGetFind = require('../../hooks/filter-on-get-find');
+const dataInCreated = require('../../hooks/data-in-created');
 
-const usersCreated = require('../../hooks/users-created');
+const generateJsonField = require('../../hooks/generate-json-field');
 
 module.exports = {
   before: {
     all: [],
     find: [authenticate('jwt')],
     get: [ authenticate('jwt') ],
-    create: [hashPassword(), usersCreated()],
+    create: [
+      hashPassword(),
+      dataInCreated({schema:['username', 'name', 'password', 'address', 'email']}),
+      generateJsonField({ schema :['name','address'] })
+    ],
     update: [ hashPassword(),  authenticate('jwt') ],
     patch: [ hashPassword(),  authenticate('jwt') ],
     remove: [ authenticate('jwt') ]
@@ -23,7 +27,16 @@ module.exports = {
     all: [protect('password')],
     find: [],
     get: [],
-    create: [],
+    create: [ function(context){
+        context.data = {
+            "name": "success",
+            "message": "Đã tạo thành công",
+            "data": {}
+        }
+
+
+        return context;
+    }],
     update: [],
     patch: [],
     remove: []
@@ -33,7 +46,11 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      function(context){
+        console.log('co bien xay ra '+context.error);
+      }
+    ],
     update: [],
     patch: [],
     remove: []
