0c2ece5993814214883e0600bbb9b3d882951126
Hồng Hải <honghai.js6@gmail.com>
2018-10-31 18:05:07 +0700

    Update src/services/blogs/blogs.service.js

--- src/services/blogs/blogs.service.js
+++ src/services/blogs/blogs.service.js
@@ -1,4 +1,6 @@
 
+/* THIS IS STUPID OBJECT : WORKING AS key WHERE principle */
+
 const iRoute = require('./blogs.class');
 const hooks = require('./blogs.hooks');
 const events = require('./blogs.events');
