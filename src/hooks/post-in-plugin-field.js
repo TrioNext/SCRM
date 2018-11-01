a123dee7efccdda47df6f5bfac734cf143278aa3
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2018-11-01 22:06:08 +0700

    Update src/hooks/post-in-plugin-field.js

--- src/hooks/post-in-plugin-field.js
+++ src/hooks/post-in-plugin-field.js
@@ -6,7 +6,16 @@ gắng thêm các field mặc định cho method POST : CREATE
 module.exports = function (options = {}) {
   return async context => {
 
-    console.log(context.params.user);
+    let { data } = context;
+    let { user } = context.params
+
+    Object.assign(data,{
+      creator_id:user.id,
+      company_id:user.company_id
+    });
+
+    
     return context;
+
   };
 };
