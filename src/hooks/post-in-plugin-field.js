126bede0f6ed9383149b7e52f8bce4953a9c034a
Hồng Hải <honghai.js6@gmail.com>
2018-11-01 17:59:16 +0700

    Update src/hooks/post-in-plugin-field.js

--- src/hooks/post-in-plugin-field.js
+++ src/hooks/post-in-plugin-field.js
@@ -1,9 +1,12 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
-// eslint-disable-next-line no-unused-vars
+/*
+gắng thêm các field mặc định cho method POST : CREATE
+*/
+
 module.exports = function (options = {}) {
   return async context => {
+
+    console.log(context.params.user);
     return context;
   };
 };
