d8420c9338a77e030f47bed4f6449134237bbc65
Hồng Hải <honghai.js6@gmail.com>
2018-11-19 17:51:25 +0700

    Update regions.model.js

--- src/models/regions.model.js
+++ src/models/regions.model.js
@@ -68,40 +68,6 @@ module.exports = function (app) {
 
 
 
-     date_created:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_modified:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-
-
-     lat:{
-       type:DataTypes.FLOAT,
-       defaultValue:0
-     },
-     lng:{
-       type:DataTypes.FLOAT,
-       defaultValue:0
-     },
-
-    json:{
-      type:DataTypes.TEXT,
-      allowNull:true,
-      defaultValue:null,
-      get(){
-        var json = this.getDataValue('json');
-        json = json !== null ? json : '{}';
-
-         // 'this' allows you to access attributes of the instance
-        return JSON.parse(json);
-      }
-    }
-
   },
   {
     indexes: [
