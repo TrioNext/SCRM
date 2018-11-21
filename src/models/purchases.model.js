13636435f8285c4a9c0f63e95599546e542673ae
Hồng Hải <honghai.js6@gmail.com>
2018-11-21 18:04:43 +0700

    Update purchases.model.js

--- src/models/purchases.model.js
+++ src/models/purchases.model.js
@@ -100,23 +100,24 @@ module.exports = function (app) {
        type:DataTypes.INTEGER,
        defaultValue:0
      },
+     
      date_created:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
 
-     date_modified:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
+   date_modified:{
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
 
-     date_deleted:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
+   date_deleted:{
+      type:DataTypes.DATE,
+      allowNull: true
+   },
+
 
      store_id:{
        type:DataTypes.INTEGER,
