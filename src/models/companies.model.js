df9e12ca03417c4e7b82ec413386b4a246f67a1a
Hồng Hải <honghai.js6@gmail.com>
2018-11-21 18:04:15 +0700

    Update companies.model.js

--- src/models/companies.model.js
+++ src/models/companies.model.js
@@ -58,19 +58,21 @@ module.exports = function (app) {
      },
 
      date_created:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_modified:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_deleted:{
-       type:'TIMESTAMP',
-       allowNull: true
-     },
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
+
+   date_modified:{
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
+
+   date_deleted:{
+      type:DataTypes.DATE,
+      allowNull: true
+   },
 
      creator_id:{
        type:DataTypes.INTEGER,
