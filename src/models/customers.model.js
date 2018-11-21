51c85037c6bcdd50f53913425bad8c6318c8c8a5
Hồng Hải <honghai.js6@gmail.com>
2018-11-21 18:04:17 +0700

    Update customers.model.js

--- src/models/customers.model.js
+++ src/models/customers.model.js
@@ -86,19 +86,21 @@ module.exports = function (app) {
 
 
      date_created:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_modified:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_deleted:{
-       type:'TIMESTAMP',
-       allowNull: true
-     },
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
+
+   date_modified:{
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
+
+   date_deleted:{
+      type:DataTypes.DATE,
+      allowNull: true
+   },
 
      /* TIME RẢNH RỔI ex: 3H pm*/
      time_available:{
