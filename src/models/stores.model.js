8ea6019cc76bcfe14d616ee9997bd61285ce907b
Hồng Hải <honghai.js6@gmail.com>
2018-11-21 18:04:45 +0700

    Update stores.model.js

--- src/models/stores.model.js
+++ src/models/stores.model.js
@@ -75,20 +75,25 @@ module.exports = function (app) {
        defaultValue:0
      },
 
+     
      date_created:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_modified:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_deleted:{
-       type:'TIMESTAMP',
-       allowNull: true
-     },
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
+
+   date_modified:{
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
+
+   date_deleted:{
+      type:DataTypes.DATE,
+      allowNull: true
+   },
+
+   
      address:{
        type:DataTypes.STRING,
        allowNull:false,
