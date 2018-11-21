0b38b855adf780dadb4846e30d85a3aaed80e580
Hồng Hải <honghai.js6@gmail.com>
2018-11-21 18:04:39 +0700

    Update payments.model.js

--- src/models/payments.model.js
+++ src/models/payments.model.js
@@ -74,19 +74,23 @@ module.exports = function (app) {
 
 
      date_created:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_modified:{
-       type:'TIMESTAMP',
-       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-       allowNull: true
-     },
-     date_deleted:{
-       type:'TIMESTAMP',
-       allowNull: true
-     },
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
+
+   date_modified:{
+      type:DataTypes.DATE,
+      defaultValue: Sequelize.NOW(),
+      allowNull: true
+   },
+
+   date_deleted:{
+      type:DataTypes.DATE,
+      allowNull: true
+   },
+
+   
 
      total_days_debt_allow:{
        type:DataTypes.TINYINT,
