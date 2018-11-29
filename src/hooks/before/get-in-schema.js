e2c09a2631366fdb4e6793040d77d38a7dc1fa94
honghaijs6 <44365604+honghaijs6@users.noreply.github.com>
2018-11-30 00:11:04 +0700

    Update get-in-schema.js

--- src/hooks/before/get-in-schema.js
+++ src/hooks/before/get-in-schema.js
@@ -19,23 +19,26 @@ module.exports = function (options = {}) {
       const paginate = context.app.get('paginate');
 
       query.p = query.p || 0 ;
+      query.offset = query.offset || 0 ;
+
       query.max = query.max || paginate.max ;
       query.sort_by =  query.sort_by || 'id';
       query.sort_type = query.sort_type || 'desc';
 
-
+      
 
       query.basicQuery = {
           order:[
             [ query.sort_by || 'id' , query.sort_type || 'desc' ]
           ],
-          offset: parseInt(query.p),
+          offset: parseInt(query.offset),
           limit: parseInt(query.max)
       };
 
       query.max === 'all' ? delete query.basicQuery.limit : '';
 
       delete query.p ;
+      delete query.offset;
       delete query.max ;
       delete query.sort_by ;
       delete query.sort_type ;
