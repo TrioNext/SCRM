64d9969c6e2e40f0dc7913cbf3089fecccc656f5
Hồng Hải <honghai.js6@gmail.com>
2018-12-27 17:57:15 +0700

    Update regions.class.js

--- src/services/regions/regions.class.js
+++ src/services/regions/regions.class.js
@@ -29,7 +29,7 @@ class iRoute extends Service {
       const query = params.query;
       const schema = this.app.get('temp_get_in_schema');
 
-      
+
 
       return query.$limit !== undefined ? await super.find(params) : await this.Model.findAndCountAll(schema);
 
@@ -56,13 +56,20 @@ class iRoute extends Service {
 
       const isUpdate = this.app.get('conditon_schema');
       ret = isUpdate;
+
       const isMethod = this.app.get('method_schema');
 
       if(isMethod.name==='success'){
          ret =  this[isMethod.method](data,params);
       }else{
 
-          ret.data = await this.Model.update(data,isUpdate.condition)
+          const isSuccess = await this.Model.update(data,isUpdate.condition);
+          ret.name = parseInt(isSuccess[0]) > 0 ? 'success' : 'fail-update' ;
+          ret.data.id = ret.condition.where.id;
+
+          delete ret.condition;
+
+
       }
 
 
@@ -70,23 +77,24 @@ class iRoute extends Service {
        return ret ;
 
     }
-
     /* cURL : DELETE */
     async remove(id, params ){
 
         /* be hooked before => data for update*/
-        const idata = this.app.get('data_del');
+        let idata = this.app.get('data_del');
 
-        idata.data =  await this.Model.update(idata.data,{
+        const isSuccess = await this.Model.update(idata.data,{
           where:{
             id:idata.id
           }
         });
 
+        idata.name = parseInt(isSuccess[0]) > 0 ? 'success' : 'fail-remove';
+        idata.data.id = idata.id ;
+
+
         return idata;
     }
-
-    /* END CRUD METHOD */
 
 
   }
