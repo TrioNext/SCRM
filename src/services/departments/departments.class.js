cf5f8032d10066219eed060fd957a07597763a45
Hồng Hải <honghai.js6@gmail.com>
2019-01-30 17:52:24 +0700

    Update departments.class.js

--- src/services/departments/departments.class.js
+++ src/services/departments/departments.class.js
@@ -44,6 +44,14 @@ class iRoute extends Service {
     /* cURL POST */
     async create(data,params){
 
+        // ROUND 1:
+        /* cleart all fields null  */
+        Object.keys(data).map((item)=>{
+          if(data[item]==='null'){
+            delete data[item];
+          }
+        });
+
 
         /* GOT HOOKED BEFOR :-> APP DATA_OUT*/
         let data_out = this.app.get('data_out');
@@ -67,6 +75,14 @@ class iRoute extends Service {
       if(isMethod.name==='success'){
          ret =  this[isMethod.method](data,params);
       }else{
+
+          /* cleart all fields null  */
+          Object.keys(data).map((item)=>{
+            if(data[item]==='null'){
+              delete data[item];
+            }
+          });
+
 
           const isSuccess = await this.Model.update(data,isUpdate.condition);
           ret.name = parseInt(isSuccess[0]) > 0 ? 'success' : 'fail-update' ;
