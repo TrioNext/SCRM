c36d68b92fec8d30217ca1b954d630e03c9da3d0
Hồng Hải <honghai.js6@gmail.com>
2019-01-30 17:52:27 +0700

    Update inventories.class.js

--- src/services/inventories/inventories.class.js
+++ src/services/inventories/inventories.class.js
@@ -43,6 +43,14 @@ class iRoute extends Service {
     async create(data,params){
 
 
+        /* cleart all fields null  */
+        Object.keys(data).map((item)=>{
+          if(data[item]==='null'){
+            delete data[item];
+          }
+        });
+
+
         /* GOT HOOKED BEFOR :-> APP DATA_OUT*/
         let data_out = this.app.get('data_out');
         data_out.data = data_out.name==='success' ?  await this.Model.create(data) : data_out.data ;
@@ -65,6 +73,13 @@ class iRoute extends Service {
       if(isMethod.name==='success'){
          ret =  this[isMethod.method](data,params);
       }else{
+
+          /* cleart all fields null  */
+          Object.keys(data).map((item)=>{
+            if(data[item]==='null'){
+              delete data[item];
+            }
+          });
 
           const isSuccess = await this.Model.update(data,isUpdate.condition);
           ret.name = parseInt(isSuccess[0]) > 0 ? 'success' : 'fail-update' ;
